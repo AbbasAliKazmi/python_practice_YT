7824fb568e80980ee027fa910ea6a92dbbcdee2b
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2024-04-04 16:14:04 +0500

    function with loops

--- class6.py
+++ class6.py
@@ -1,43 +1,70 @@
-#<............function........>
+# #<............function........>
 
 
-# def calc_sum(a,b):
-#     sum = a + b
-#     print(sum)
-#     return 
+# # def calc_sum(a,b):
+# #     sum = a + b
+# #     print(sum)
+# #     return 
     
-# calc_sum(2,5)    
+# # calc_sum(2,5)    
 
-# def num_avg( a, b, c):
-#     sum = a + b + c
-#     avg = sum / 3
-#     print(avg)
-#     return
+# # def num_avg( a, b, c):
+# #     sum = a + b + c
+# #     avg = sum / 3
+# #     print(avg)
+# #     return
 
-# num_avg(2,6,8)
-
-
-#<..............function types ............>
-# Built in functions                                User define function
-#  print()                                             jjo hum bnart hyn
-#   len()
-#   type()
-#   range()
+# # num_avg(2,6,8)
 
 
-
-#<................Default parameters......>
-
-
-#lets practice
-
-
-liist = [ 4,5,6,0,5,0]
-heroes = ["thor" , "ironman" ,"spiderman" , "cap america"]
+# #<..............function types ............>
+# # Built in functions                                User define function
+# #  print()                                             jjo hum bnart hyn
+# #   len()
+# #   type()
+# #   range()
 
 
-def len_list(list):
-    print(len(list))
+
+# #<................Default parameters......>
 
 
-len_list(heroes)
+# #lets practice
+
+
+# # liist = [ 4,5,6,0,5,0]
+# # heroes = ["thor" , "ironman" ,"spiderman" , "cap america"]
+
+
+# # def len_list(list):
+# #     print(len(list))
+
+# # len_list(heroes)
+
+
+# # def print_list(leest):
+# #     for items in leest:
+# #         print(items, end=" ")
+                
+# # print_list(heroes)        
+
+
+# USD to PKR
+
+# def converter(usd_val):
+#     pkr_value = usd_val* 280
+#     print(usd_val,"Dollars = ", pkr_value,"PKR")
+    
+# converter(5)  
+
+
+#lets practice homework
+
+def int_check(n):
+    n = int(n)  # Convert input to integer
+    if n % 2 == 0:
+        print("Number is Even")
+    else:
+        print("Number is odd")
+
+int_check(input("Enter a number: "))      
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,4 @@
-def calc_sum(a,b):
-#     sum = a + b
-#     print(sum)
-#     return 
-    
-# calc_sum(2,5) 
+def len_list(list):
+#     print(len(list))
+
+# len_list(heroes)
