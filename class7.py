0ccfb6ea50c18739d2d66c210371ce6962ca97f8
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2024-04-11 20:01:31 +0500

    class 7 ,file read by characters no. or by line no.

--- class7.py
+++ class7.py
@@ -9,3 +9,11 @@ data = f.read()
 print(data)
 print(type(data))
 f.close()
+
+#File open and read with characters or by line
+
+f = open("democlass7.txt", "r")
+data = f.read(10)
+print(data)
+print(type(data))
+f.close()
